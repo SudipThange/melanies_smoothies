c089f71e690c47d405f1526ef975795e04418f98
Sudip Santosh Thange <101926866+SudipThange@users.noreply.github.com>
2026-05-20 11:56:47 +0530

    Modify dataframe display and add pandas conversion
    Comment out the original dataframe display and stop calls, and add a new pandas dataframe display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,14 +22,18 @@ session = cnx.session()
 #--------------------------------
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col("FRUIT_NAME"), col("SEARCH_ON"))
-st.dataframe(data=my_dataframe, use_container_width = True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width = True)
+#st.stop()
                                                                                              
 ingredients_list = st.multiselect(
     'Choose upto 5 ingredients'
     ,my_dataframe
     ,max_selections = 5
 )
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 if ingredients_list:
     ingredients_string = ''
