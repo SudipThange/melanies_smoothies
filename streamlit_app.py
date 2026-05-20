298932cbb5c9127f0ea1be52e06afea18bed7d6b
Sudip Santosh Thange <101926866+SudipThange@users.noreply.github.com>
2026-05-20 11:58:06 +0530

    Fix indentation and update search functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,10 @@ if ingredients_list:
     
     for fruits_chose in ingredients_list:
         ingredients_string += fruits_chose + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruits_chose + 'Nutrition Information')
       
         smoothiefroot_response = requests.get(
